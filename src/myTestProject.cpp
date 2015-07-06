39884864bcaba72c6b257ee8ca759fd5710ca5f3
danielwillem85 <danielp@NB10636.citogroep.nl>
2015-07-06 15:10:51 +0200

    Initial commit

---
+++ src/myTestProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : myTestProject.cpp
+// Author      : D.W. van der Palm
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
