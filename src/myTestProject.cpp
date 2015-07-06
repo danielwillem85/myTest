60b3017018a968dd98d394427a2b0534f0e770dc
danielwillem85 <danielp@NB10636.citogroep.nl>
2015-07-06 15:11:37 +0200

    Changed output text

--- src/myTestProject.cpp
+++ src/myTestProject.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello world, GIT is working!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
